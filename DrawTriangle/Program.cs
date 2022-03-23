a11baca6551488eeb8aac2fce2f92c1695551a8f
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 11:02:37 +0300

    Added DrawTriangle project

---
+++ DrawTriangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DrawTriangle
+{
+    class Program
+    {
+          
+        static void Main(string[] args)
+        {
+            /*
+            Kullanıcıdan alınan boyut bilgisine göre console'a Üçgen çizen uygulama
+            */
+            Console.Write("Boyut bilgisini girin : ");
+            int size=int.Parse(Console.ReadLine());
+            Draw draw =new Draw();
+            draw.DrawTriangle(size);
+        }
+    }
+
+    /*
+    Draw isimli çizim nesnesi
+    */
+     class Draw 
+    {
+        /*
+        Boyut bilgisine göre eşkenar üçken çizen metot
+        */
+        public void DrawTriangle(int size)
+        {
+           
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = size; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
