7088aa7580e6fd6095bdbabefe3c5c156bdf27a0
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-01 21:02:07 +0300

    enum kullanımı

---
+++ CSharp-101/csharp_enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık = 32;
+            if (sıcaklık <= (int)HavaDurumu.Normal)
+                System.Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+                System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık < (int)HavaDurumu.CokSıcak)
+                System.Console.WriteLine("Hadi dışarıya çıkalım");
+
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
