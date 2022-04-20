aa8b89d9eaec31b82c721854dc6f0504a2ae753c
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-20 03:06:57 +0300

    Added Get and GetById endpoint

---
+++ .Net Core/Pratikler/BookStore/WebApi/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int GenreId { get; set; }
+        public int PageCount { get; set; }
+        public DateTime PublishDate { get; set; }
+        
+    }
+}
---
+++ .Net Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]s")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        
+        private static List<Book> books = new List<Book> ()
+        {
+        new Book(){Id=1,Title="LeanStartup",GenreId=1,/*Personel Growth*/PageCount=200,PublishDate=new DateTime(2010,12,21)},
+        new Book(){Id=2,Title="Herland",GenreId=2,/*Science Fiction*/PageCount=250,PublishDate=new DateTime(2010,12,21)},
+        new Book(){Id=3,Title="Dune",GenreId=2,/*Science Fiction*/PageCount=540,PublishDate=new DateTime(2010,12,21)}
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = books.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = books.Where(x => x.Id == id).SingleOrDefault();
+
+            return book;
+        }
+        //[HttpGet]
+        //public Book Get([FromQuery]string id)
+        //{
+        //    var book = books.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
+
+        //    return book;
+        //}
+    }
+}
