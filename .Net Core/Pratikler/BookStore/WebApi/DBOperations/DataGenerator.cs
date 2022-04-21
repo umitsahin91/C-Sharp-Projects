de82e1079be82a8b8a273424741fe4183a94cb0b
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-21 06:18:23 +0300

    auto increment id

--- .Net Core/Pratikler/BookStore/WebApi/DBOperations/DataGenerator.cs
+++ .Net Core/Pratikler/BookStore/WebApi/DBOperations/DataGenerator.cs
@@ -18,9 +18,9 @@ namespace WebApi.DBOperations
                     return;
                 }
                 context.Books.AddRange(
-                    new Book() { Id = 1, Title = "LeanStartup", GenreId = 1,/*Personel Growth*/PageCount = 200, PublishDate = new DateTime(2010, 12, 21) },
-                    new Book() { Id = 2, Title = "Herland", GenreId = 2,/*Science Fiction*/PageCount = 250, PublishDate = new DateTime(2010, 12, 21) },
-                    new Book() { Id = 3, Title = "Dune", GenreId = 2,/*Science Fiction*/PageCount = 540, PublishDate = new DateTime(2010, 12, 21) }
+                    new Book() { Title = "LeanStartup", GenreId = 1,/*Personel Growth*/PageCount = 200, PublishDate = new DateTime(2010, 12, 21) },
+                    new Book() { Title = "Herland", GenreId = 2,/*Science Fiction*/PageCount = 250, PublishDate = new DateTime(2010, 12, 21) },
+                    new Book() { Title = "Dune", GenreId = 2,/*Science Fiction*/PageCount = 540, PublishDate = new DateTime(2010, 12, 21) }
                     );
                 context.SaveChanges();
             }
