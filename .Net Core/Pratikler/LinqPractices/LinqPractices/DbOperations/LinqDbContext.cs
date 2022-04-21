01a63df3bbb26111f6fd5f944c1a305391e44b84
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-21 07:29:42 +0300

    linq practices

---
+++ .Net Core/Pratikler/LinqPractices/LinqPractices/DbOperations/LinqDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LinqPractices.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractices.DbOperations
+{
+    class LinqDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "LinqDatabase");
+        }
+    }
+}
---
+++ .Net Core/Pratikler/LinqPractices/LinqPractices/Program.cs
@@ -0,0 +1,78 @@
+﻿using LinqPractices.DbOperations;
+using LinqPractices.Entities;
+using System;
+using System.Linq;
+
+namespace LinqPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGenerator.Initialize();
+            LinqDbContext _context = new LinqDbContext();
+            var students = _context.Students.ToList<Student>();
+
+            //Find()
+            Console.WriteLine("***Find***");
+            var student = _context.Students.Where(s => s.StudentId == 1).FirstOrDefault();
+            Console.WriteLine(student.Name);
+
+            student = _context.Students.Find(1);
+            Console.WriteLine(student.Name);
+
+            //FirstOrDefault()
+            Console.WriteLine("***FirstOrDefault***");
+            student = _context.Students.Where(s => s.Surname == "Arda").FirstOrDefault();
+            Console.WriteLine(student.Name);
+
+            student = _context.Students.FirstOrDefault(s => s.Surname == "Arda");
+            Console.WriteLine(student.Name);
+
+            //SingleOrDefault()
+            Console.WriteLine("***SingleOrDefault***");
+            student = _context.Students.SingleOrDefault(s => s.Name == "Deniz");
+            Console.WriteLine(student.Surname);
+
+            //ToList()
+            Console.WriteLine("***ToList***");
+            var studentList = _context.Students.Where(s => s.ClassId == 2).ToList();
+            Console.WriteLine(studentList.Count);
+            foreach (var s in studentList)
+            {
+                Console.WriteLine(s.Name);
+            }
+
+            //OrderBy()
+            Console.WriteLine("***OrderBy***");
+
+            studentList = _context.Students.OrderBy(s => s.StudentId).ToList();
+            foreach (var s in studentList)
+            {
+                Console.WriteLine(s.StudentId+ "-"+s.Name+" " +s.Surname);
+            }
+
+            //OrderByDescending()
+            Console.WriteLine("***OrderByDescending***");
+
+            studentList = _context.Students.OrderByDescending(s => s.StudentId).ToList();
+            foreach (var s in studentList)
+            {
+                Console.WriteLine(s.StudentId + "-" + s.Name + " " + s.Surname);
+            }
+
+            //Anonymous Object Result
+            Console.WriteLine("***Anonymous Object Result***");
+
+            var anonymousObject = _context.Students
+                .Where(s => s.ClassId == 2)
+                .Select(x => new { Id = x.StudentId, FullName = x.Name + " " + x.Surname });
+
+            foreach (var obj in anonymousObject)
+            {
+                Console.WriteLine(obj.Id+"-"+obj.FullName);
+            }
+
+        }
+    }
+}
