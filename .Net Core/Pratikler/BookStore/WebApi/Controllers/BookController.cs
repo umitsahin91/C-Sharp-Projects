2c71d1ba2c9f4d08d8b4c39bde023d03be30cfc8
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-21 05:42:30 +0300

    Entity.Framework.Core included

--- .Net Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
+++ .Net Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
@@ -14,9 +14,7 @@ namespace WebApi.Controllers
         
         private static List<Book> books = new List<Book> ()
         {
-        new Book(){Id=1,Title="LeanStartup",GenreId=1,/*Personel Growth*/PageCount=200,PublishDate=new DateTime(2010,12,21)},
-        new Book(){Id=2,Title="Herland",GenreId=2,/*Science Fiction*/PageCount=250,PublishDate=new DateTime(2010,12,21)},
-        new Book(){Id=3,Title="Dune",GenreId=2,/*Science Fiction*/PageCount=540,PublishDate=new DateTime(2010,12,21)}
+       
         };
 
         [HttpGet]
---
+++ .Net Core/Pratikler/BookStore/WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+                context.Books.AddRange(
+                    new Book() { Id = 1, Title = "LeanStartup", GenreId = 1,/*Personel Growth*/PageCount = 200, PublishDate = new DateTime(2010, 12, 21) },
+                    new Book() { Id = 2, Title = "Herland", GenreId = 2,/*Science Fiction*/PageCount = 250, PublishDate = new DateTime(2010, 12, 21) },
+                    new Book() { Id = 3, Title = "Dune", GenreId = 2,/*Science Fiction*/PageCount = 540, PublishDate = new DateTime(2010, 12, 21) }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
