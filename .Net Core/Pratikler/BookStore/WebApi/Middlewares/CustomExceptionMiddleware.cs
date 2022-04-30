a29262e48776fa86a72685a9c229f19e60629829
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-30 09:01:44 +0300

    added logger service

--- .Net Core/Pratikler/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ .Net Core/Pratikler/BookStore/WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -7,16 +7,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using WebApi.Services;
 
 namespace WebApi.Middlewares
 {
     public class CustomExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILoggerService _loggerService;
 
-        public CustomExceptionMiddleware(RequestDelegate next)
+
+
+        public CustomExceptionMiddleware(RequestDelegate next, ILoggerService loggerService)
         {
             _next = next;
+            _loggerService = loggerService;
         }
 
         public async Task Invoke(HttpContext context)
@@ -25,12 +30,12 @@ namespace WebApi.Middlewares
             try
             {
                 
-                string message = $"[Request] HTTP {context.Request.Method} - {context.Request.Path}";
-                Console.WriteLine(message);
+                string message = $"[Request]  HTTP {context.Request.Method,5} - {context.Request.Path,7}";
+                _loggerService.Write(message);
                 await _next(context);
                 watch.Stop();
-                message = $"[Response] HTTP {context.Request.Method} - {context.Request.Path} responded {context.Response.StatusCode} in {watch.Elapsed.TotalMilliseconds} ms";
-                Console.WriteLine(message);
+                message = $"[Response] HTTP {context.Request.Method,5} - {context.Request.Path,7} responded {context.Response.StatusCode,5} in {watch.Elapsed.TotalMilliseconds,10} ms";
+                _loggerService.Write(message);
             }
             catch (Exception ex)
             {
@@ -47,8 +52,8 @@ namespace WebApi.Middlewares
             context.Response.ContentType = "applicationjson";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            string message = $"[ERROR] HTTP {context.Request.Method} - {context.Response.StatusCode} - Error Message : {ex.Message} in {watch.Elapsed.TotalMilliseconds} ms";
-            Console.WriteLine(message);
+            string message = $"[ERROR] HTTP {context.Request.Method,5} - {context.Response.StatusCode,5} - Error Message : {ex.Message} in {watch.Elapsed.TotalMilliseconds,10} ms";
+            _loggerService.Write(message);
 
             var result = JsonConvert.SerializeObject(new { error = ex.Message },Formatting.None);
             return context.Response.WriteAsync(result);
@@ -61,6 +66,7 @@ namespace WebApi.Middlewares
         public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder builder)
         {
             return builder.UseMiddleware<CustomExceptionMiddleware>();
+            
         }
     }
 }
---
+++ .Net Core/Pratikler/BookStore/WebApi/Services/DbLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Services
+{
+    public class DbLogger : ILoggerService
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"[Db Logger : {message}]");
+        }
+    }
+}
